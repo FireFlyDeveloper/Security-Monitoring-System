52f6aeac9abc6e9edcd20a954239164d4d55588f
Kim <saludeskimdev@gmail.com>
2025-06-26 15:25:10 +0800

    fix: update allowed development origins in Next.js configuration

--- src/app/api/device/calibrate/route.js
+++ src/app/api/device/calibrate/route.js
@@ -3,22 +3,19 @@ import { NextResponse } from "next/server";
 
 export async function POST(request) {
   try {
-    const { ids } = await request.json();
+    const { mac } = await request.json();
     const cookie = await cookies();
     const session = cookie.get("session");
-    const response = await fetch("http://security.local:8080/api/devices/delete-devices", {
+    const response = await fetch(`http://security.local:8080/api/position/update/${encodeURIComponent(mac)}`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
             "Authorization": session ? `Bearer ${session.value}` : "",
-        },
-        body: JSON.stringify({
-            ids: [ ...ids ]
-        })
+        }
     });
 
     if (!response.ok) {
-        return NextResponse.json({ error: "Failed to add device" }, { status: response.status });
+        return NextResponse.json({ error: "Failed to calibrate device" }, { status: response.status });
     }
 
     return NextResponse.json({ message: "Devices removed successfully" }, { status: 200 });
