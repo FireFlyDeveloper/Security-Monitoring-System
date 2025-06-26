f80ed1e5fc40ac60b79891bde5b616049c0e9909
Kim <saludeskimdev@gmail.com>
2025-06-26 14:37:49 +0800

    fix: update allowed development origins in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+  allowedDevOrigins: ['http://security.local:3000'],
 };
 
 export default nextConfig;
